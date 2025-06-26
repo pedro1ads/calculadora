e47bd8c7c90c7226d3c57d8887c1c5decb31d476
pedro1ads <pedro.polli@escola.pr.gov.br>
2025-06-26 08:42:47 -0300

    Add files via upload

---
+++ calculaora/script.js
@@ -0,0 +1,95 @@
+function soma(){
+
+let numero_um = parseInt (document.getElementById("primeiro_numero"). value);
+let numero_dois = parseInt (document. getElementById ("segundo_numero"). value);
+let soma_numero = numero_um + numero_dois ;
+
+document.getElementById("resultado"). textContent = "Resultado: " + soma_numero;
+
+
+}
+
+function subtrai(){
+let numero_um = parseInt(document.getElementById("primeiro_numero"). value);
+let numero_dois = parseInt(document. getElementById("segundo_numero"). value);
+let subtrai_numero = numero_um - numero_dois ;
+
+document.getElementById("resultado"). textContent = "Resultado: " + subtrai_numero;
+
+}
+
+function multiplicar(){
+let numero_um = parseInt(document.getElementById("primeiro_numero"). value);
+let numero_dois = parseInt(document. getElementById("segundo_numero"). value);
+let multiplicar_numero = numero_um - numero_dois ;
+
+document.getElementById("resultado"). textContent = "Resultado: " + multiplicar_numero;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
